83e34d1a6defa119e032400f805717523d1236e6
Boateng Dickson <89385798+dboatengg@users.noreply.github.com>
2022-04-06 10:22:49 +0000

    fix header section

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const body = document.getElementsByTagName('body');
+const navMenu = document.querySelector(".nav__menu");
+const body = document.getElementsByTagName("body");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    document.body.classList.toggle('lock-scroll')
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  document.body.classList.toggle("lock-scroll");
 }
 
-
-const navLink = document.querySelectorAll(".nav-menu__item");
+const navLink = document.querySelectorAll(".nav__item");
 navLink.forEach((n) => n.addEventListener("click", removeOverflow));
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-    body.classList.toggle('opened');
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  body.classList.toggle("opened");
 }
 
 //add the overflow function
 function removeOverflow() {
-    document.body.classList.remove('lock-scroll')
-  }
+  document.body.classList.remove("lock-scroll");
+}
